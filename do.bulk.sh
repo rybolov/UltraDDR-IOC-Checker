b23f60f2a962e6c013fc697597f1d7f9a681bb76
Michael Smith <rybolov@rybolov.net>
2023-06-01 06:39:11 -0400

    do.bulk.sh: -Changed python file name. -Enabled git add for .txt files (IoC files)

--- do.bulk.sh
+++ do.bulk.sh
@@ -4,8 +4,8 @@ for f in ./data/*.txt  # All advisory lists should be a .txt file
 do
   echo "Processing $f"
   # take action on each file. $f store current file name
-  python3 ./cti-seeder.py -i $f
+  python3 ./ddr-ioc-checker.py -i $f
 done
-#git add ./data/*.txt  # All advisory lists
+git add ./data/*.txt  # All advisory lists
 git add ./data/*.csv   # All output files
 git commit -m "Updated analysis CSV files."
