4875fa97189fd54b70e054a29a1f25879a505d52
Michael Smith <michael.smith@neustarsecurityservices.com>
2023-06-06 11:38:34 -0700

    ddr-ioc-checker.py: -PEP8 fixes -Added better handling for NXDOMAIN and PTR queries -Added command flag to run in serial mode for better troubleshooting

--- ddr-ioc-checker.py
+++ ddr-ioc-checker.py
@@ -12,7 +12,8 @@ import csv
 
 
 if not os.path.exists('config.py'):
-    exit('Error, you haven\'t set up a configuration.\nPlease copy config.py.example to config.py and change the ClientID.')
+    exit('Error, you haven\'t set up a configuration.\n\
+    Please copy config.py.example to config.py and change the ClientID.')
 else:
     import config
     config = config.Config()
@@ -21,7 +22,6 @@ else:
 
 generationdate = datetime.datetime.now().strftime("%Y.%m.%d %I:%M:%S %p")
 today = datetime.datetime.now().strftime("%Y-%m-%d")
-
 
 
 print('''
@@ -35,7 +35,11 @@ print('''
   \\___|_||_|___\\___|_|\\_\\___|_|_\\
 ''')
 
+# Todo: Make PTRs work
+
 # ----------Begin Input Validation----------
+
+
 def is_valid_file(parser, filename):
     if not os.path.exists(filename):
         parser.error("The file %s does not exist!" % filename)
@@ -43,11 +47,14 @@ def is_valid_file(parser, filename):
     else:
         return filename
 
+
 parser = argparse.ArgumentParser(description='Send queries for CTI IOC to UltraDDR.')
 parser.add_argument('-i', '--input', dest='filename', required=False, metavar='FILE',
                     help='Input file with one IOC per line.', type=lambda x: is_valid_file(parser, x))
 parser.add_argument('--strict', action='store_true', help="Do not run validation if there are bad lines.\
 This ignores comments.", default=False)
+parser.add_argument('--serial', action='store_true', help="Process in serial instead of parallel. \
+This helps in troubleshooting but is slower.", default=False)
 args = parser.parse_args()
 # ----------End Input Validation----------
 
@@ -61,12 +68,12 @@ class IOCList:
         self.allvalid = True
         self.failedlines = []
         self.csv = \
-        [
-            ['Date Generated:  ' + generationdate],
-            ['Questions, comments, or complaints: contact threat-intel@vercara.com'],
-            [],
-            ['Query Name', 'UltraDDR Status']
-        ]
+            [
+                ['Date Generated:  ' + generationdate],
+                ['Questions, comments, or complaints: contact threat-intel@vercara.com'],
+                [],
+                ['Query Name', 'UltraDDR Status']
+            ]
 
     def __repr__(self):
         """Return string representation of everything."""
@@ -75,7 +82,8 @@ class IOCList:
     def get_iocs_from_file(self):
         linenumber = 0
         if not self.filename:
-            exit('Error, no filename was passed.\nPlease use the -i flag to specify a file.\nUse --help for more information.')
+            exit('Error, no filename was passed.\nPlease use the -i flag to specify a file.\
+            \nUse --help for more information.')
         else:
             print('Openening file:', self.filename)
             with open(self.filename) as f_open:
@@ -87,15 +95,21 @@ class IOCList:
                 line = line.strip()  # Remove whitespace
                 line = line.rstrip('.')  # For any lists that use DNS-style FQDNs that end with a dot.
                 line = line.lower()  # Use all lower-case
-                line = re.sub('\[\.\]', '.', line)  # Most CTI list domains as foo[.]com to keep you from clicking on them.
-                line=re.sub('^h[tx]{2}ps*://','', line)  # Remove "http://", "https://" "hxxp://" and "hxxps://"
-                line = re.sub('/.*$', '', line)  # Remove "/path/and/anything/else/here" and rely on regex being "greedy"
+
+                # Most CTI list domains as foo[.]com to keep you from clicking on them.
+                line = re.sub('\[\.\]', '.', line)
+
+                # Remove "http://", "https://" "hxxp://" and "hxxps://"
+                line = re.sub('^h[tx]{2}ps*://', '', line)
+
+                # Remove "/path/and/anything/else/here" and rely on regex being "greedy"
+                line = re.sub('/.*$', '', line)
                 print(line)
                 if re.search('^#', line):
                     print('Line is a comment: ', line)
                 elif re.search('^$', line):
                     print('Line is empty: ', line)
-                elif re.search('\s', line): # Disqualify because of spaces
+                elif re.search('\s', line):  # Disqualify because of spaces
                     print('Line contains a space in the middle of it,', line)
                     self.allvalid = False
                     self.failedlines.append(line)
@@ -108,7 +122,7 @@ class IOCList:
                 elif re.search('^[a-z0-9\-\.]*$', line):  # Domain/FQDN allowable characters is allowed
                     print('Line is valid FQDN/domain: ', line)
                     self.IOCnames[line] = IOCName(line)
-                elif re.search('^.+@[a-z0-9\-\.]*$', line): # Email address is allowed
+                elif re.search('^.+@[a-z0-9\-\.]*$', line):  # Email address is allowed
                     print('Line is valid email: ', line)
                     self.IOCnames[line] = IOCName(line)
                 else:
@@ -133,7 +147,8 @@ class IOCList:
             ioc.get_ddr()
 
     def get_ddr_multiprocessing(self):
-        Parallel(n_jobs=5, require='sharedmem')(delayed(get_ddr_multiprocessing)(iocname) for iocname in self.IOCnames.values())
+        Parallel(n_jobs=5, require='sharedmem')(delayed(get_ddr_multiprocessing)(iocname)
+                                                for iocname in self.IOCnames.values())
 
 
 class IOCName:
@@ -155,14 +170,18 @@ class IOCName:
                 http = urllib3.PoolManager()
                 if re.search('@', self.iocname):
                     queryurl = config.ProviderURL + re.sub('^.*@', '', self.iocname) + '&type-'
+                    self.type = 'Email'
                 else:
                     queryurl = config.ProviderURL + self.iocname + '&type='
                 if re.search('^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$', self.iocname):  # IPv4 Address
                     queryurl += 'PTR'
+                    self.type = 'IPV4'
                 elif re.search('^([0-9a-fA-F]{0,8}:){7}[0-9a-fA-F]{0,8}$', self.iocname):  # IPv6 Address
                     queryurl += 'PTR'
+                    self.type = 'IPV6'
                 else:
                     queryurl += 'A'
+                    self.type = 'FQDN'
                 print(queryurl)
                 req = http.request('GET', queryurl,
                                    headers={
@@ -177,8 +196,8 @@ class IOCName:
                 time.sleep(looper * 2)
             except urllib3.exceptions.HTTPError as e:
                 if re.search('certificate verify failed: unable to get local issuer certificate', e.reason):
-                    print('Couldn\'t find the CA Certs, import ultraddr-ca-cert.pem and run '\
-                          '\'<phythonhome/Install Certificates.command\'')
+                    print('Couldn\'t find the CA Certs, import ultraddr-ca-cert.pem and run \
+                          \'<phythonhome/Install Certificates.command\'')
                 print(e.reason)
                 print("HTTP error. Resending....")
                 time.sleep(looper * 2)
@@ -196,35 +215,42 @@ class IOCName:
         # print(json.dumps(ddr_results, indent=4))
 
         self.rawresults = json.dumps(ddr_results)
-        if 'Answer' in ddr_results.keys():
-            print(json.dumps(ddr_results['Answer'][0]['data'], indent=4))
-            if ddr_results['Answer'][0]['data'] == '20.13.128.62':
-                self.status = 'Blocked'
-                print('Blocked')
-            else:
-                self.status = 'Not Blocked'
-                print('Not Blocked')
+        if ddr_results['Status'] == 0:  # 0 means we got an answer.
+            if 'Answer' in ddr_results.keys():
+                print(json.dumps(ddr_results['Answer'][0]['data'], indent=4))
+                if ddr_results['Answer'][0]['data'] == '20.13.128.62':
+                    self.status = 'Blocked'
+                    print('Blocked')
+                else:
+                    self.status = 'Not Blocked'
+                    print('Not Blocked')
             # print(self.status)
+        elif self.type in ['IPV4', 'IPV6']:
+            self.status = 'PTR'
+        elif ddr_results['Status'] == 3:  # 3 is NXDOMAIN
+            self.status = 'NXDOMAIN'
         else:
-            self.status = "NXDOMAIN"
+            self.status = "Error"
         # print(self)
+
 
 def get_ddr_multiprocessing(ioc):
     ioc.get_ddr()
 
 
-
-
-
 def obj_dict(obj):  # Needed for the json.dumps() call in the __repr__ of the classes.
     return obj.__dict__
+
 
 def main():
     fullfile = IOCList()
     fullfile.filename = args.filename
     fullfile.get_iocs_from_file()
     # fullfile.get_ddr_serial()
-    fullfile.get_ddr_multiprocessing()
+    if args.serial:
+        fullfile.get_ddr_serial()
+    else:
+        fullfile.get_ddr_multiprocessing()
     fullfile.makeCSV()
     # print(json.dumps(fullfile.csv, indent=4))
     # print(fullfile)
@@ -233,10 +259,9 @@ def main():
         writer = csv.writer(f)
         writer.writerows(fullfile.csv)
 
+
 def readfile(filename):
     pass
-
-
 
 
 if __name__ == "__main__":
